b334fc60f22b18cfc0f20aa9d730feef3385f5fb
mohosin16103308 <hamza1610330816@gmail.com>
2019-08-23 15:06:06 +0600

    POS added product,supplier,category model and migration

---
+++ database/migrations/2019_08_23_054337_create_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('category_id')->unsigned();
+            $table->float('price');
+            $table->float('sell_price');
+            $table->string('code');
+            $table->integer('supplier_id')->unsigned();
+            $table->string('img');
+            $table->string('unit');
+            $table->string('weight');
+            $table->integer('quantity');
+            $table->string('company');
+            $table->string('tax');
+            $table->string('product_type');
+            $table->string('method');
+            $table->string('note');
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');;
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
